4303d8b150bfd9f40c0dba7c85d2223049b2e389
VO VIET DUNG <dungvv117@gmail.com>
2021-04-11 21:44:13 +0700

    update 11_04

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import LoginScreen from './Components/LoginScreen';
+//import './Components/LoginScreen.js';
+import NewsScreen from './Components/NewsScreen';
+import {NavLink} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! Việt Nam quyết thắng đại dịch Covid 19
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="login">
+            <LoginScreen />
+          </div>
+          <div className="news">
+            <NewsScreen />
+          </div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <NavLink className="navbar-brand" to="/#">
+              Navbar
+            </NavLink>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav mr-auto">
+                      <li className="nav-item">
+                        <NavLink className="nav-link" to="/#">
+                          Home
+                        </NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink className="nav-link" to="/#">
+                          Features
+                        </NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink className="nav-link" to="/#">
+                          Pricing
+                        </NavLink>
+                      </li>
+                    </ul>
+                    <NavLink className="btn btn-outline-info my-2 my-sm-0" to="/login">
+                      Login
+                    </NavLink>
+                    <NavLink
+                      className="btn btn-outline-info my-2 my-sm-0 ml-3"
+                      to="/register"
+                    >
+                      Register
+                    </NavLink>
+                  </div>
+                </nav>
     </div>
   );
 }
---
+++ src/Components/NewsScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+NewsScreen.propTypes = {
+    
+};
+
+function NewsScreen(props) {
+    return (
+        <div>
+        <p>
+            NewsScreen
+        </p>
+            <img src = "Images/news-0.png " alt = " "/>
+        </div>
+    );
+}
+
+export default NewsScreen;
