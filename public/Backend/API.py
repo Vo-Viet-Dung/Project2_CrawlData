dc66f25b88e0a1561d2ec75ea524860fca32fbe1
VO VIET DUNG <dungvv117@gmail.com>
2021-06-01 22:30:17 +0700

    update v2 1/6/2021

--- public/Backend/API.py
+++ public/Backend/API.py
@@ -11,6 +11,7 @@ try:
     # db = mongoengine()
     app.config['MONGO_DBNAME'] = 'mydatabase'
     app.config['MONGO_URI'] = 'mongodb://localhost:27017/mydatabase'
+    app.config['JSON_AS_ASCII'] = False  # cho phep doc chuoi utf8
     mongo = PyMongo(app)
 except:
     print(Exception)
@@ -27,9 +28,59 @@ def get_News():
         for i in col.find():
             title = i["title"].decode()
             content = i["content"].decode()
+            abstract = i["abstract"].decode()
             o.append({"_ID": str(ObjectId(
-                i["_id"])), "link": i["link"], "title": str(title), "content": str(content)})
+                i["_id"])), "link": i["link"], "title": str(title), "abstract": str(abstract), "content": str(content), "image": i["image"]})
+            json.dumps(o, ensure_ascii=False).encode('utf8')
         return jsonify(o)
+    except:
+        print(Exception)
+
+# ---------------------------------------------------------
+
+
+@app.route("/laws", methods=["GET"])
+def get_LawNews():
+    try:
+        col = db.lawNews
+        o = []
+        for i in col.find():
+            title = i["title"].decode()
+            content = i["content"].decode()
+            abstract = i["abstract"].decode()
+            o.append({"_ID": str(ObjectId(
+                i["_id"])), "link": i["link"], "title": str(title), "abstract": str(abstract), "content": str(content), "image": i["image"]})
+            json.dumps(o, ensure_ascii=False).encode('utf8')
+        return jsonify(o)
+    except:
+        print(Exception)
+# ---------------------------------------------------------------
+
+
+@app.route("/business", methods=["GET"])
+def get_BusinessNews():
+    try:
+        col = db.businessNews
+        o = []
+        for i in col.find():
+            title = i["title"].decode()
+            content = i["content"].decode()
+            abstract = i["abstract"].decode()
+            o.append({"_ID": str(ObjectId(
+                i["_id"])), "link": i["link"], "title": str(title), "abstract": str(abstract), "content": str(content), "image": i["image"]})
+            json.dumps(o, ensure_ascii=False).encode('utf8')
+        return jsonify(o)
+    except:
+        print(Exception)
+# ---------------------------------------------------------------
+
+
+@app.route("/deleteNews", methods=["DELETE"])
+def delete_News(id):
+    try:
+        col = db.news
+        col.delete_one({"_id": ObjectId(id)})
+        return jsonify({"message": "delete"})
     except:
         print(Exception)
 
